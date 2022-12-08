9b2889db9b3f022a50a5352b2d146966551d5881
chunwei <chunwei@uchicago.edu>
2022-12-07 22:20:14 -0600

    dataframe layout in jupyter directly

--- src/connect.py
+++ src/connect.py
@@ -274,10 +274,10 @@ def print_list(dir):
 
 DATASET_INFO = {
     "nychealth.csv": "NYC Health Covid data with 1011 rows x 67 columns",
-    "covid_confirmed_usafacts.csv": "USA Covid confirmed cases with 3194 rows x 557 columns",
-    "covid_deaths_usafacts.csv": "USA Covid death cases with 3194 rows x 557 columns",
+    "covid_confirmed_usafacts.csv": "USA Covid confirmed cases with 3194 rows x 557 columns (truncated columns)",
+    "covid_deaths_usafacts.csv": "USA Covid death cases with 3194 rows x 557 columns (truncated columns)",
     "covid_tracking.csv": "Covid tracking data with 20781 rows x 56 columns",
-    "usafacts_hist.csv": "Fact Covid data (adm2) with 1800367 rows x 9 columns"
+    "usafacts_hist.csv": "Fact Covid data (adm2) with 1800367 rows x 5 columns"
 }
 def print_df(dir):
     print("We have a set of datasets:")
