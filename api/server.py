efc49844b70a74020a62bde0215224886d478e43
Markos Markakis <mmarkakis98@gmail.com>
2023-01-25 10:13:18 -0500

    Bump version

--- api/server.py
+++ api/server.py
@@ -21,7 +21,7 @@ def build_api(*args) -> FastAPI:
         title="Annotation API",
         description="MIT annotation API",
         docs_url="/",
-        version="0.0.1", 
+        version="0.0.2", 
         openapi_tags=tags_metadata
     )
     origins = [
