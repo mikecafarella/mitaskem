da8321c2b2322d5af8988c6a4587032294c908fd
chunwei <chunwei@uchicago.edu>
2023-01-26 10:50:20 -0500

    adding new img2mml call

--- src/connect.py
+++ src/connect.py
@@ -1,9 +1,16 @@
+import asyncio
+import io
 import os
 import re
 import pandas as pd
 from cryptography.fernet import Fernet
 import openai
 from openai import OpenAIError
+import pix2tex.api.app as p2t
+from PIL import Image
+# from pix2text import Pix2Text
+import gpt
+
 from util import *
 from gpt_interaction import *
 from mira_dkg_interface import *
@@ -27,6 +34,92 @@ def index_text(text: str) -> str:
             break
         idx_text = idx_text + ('%d\t%s\n' % (i, line))
     return idx_text
+
+
+
+
+def get_gpt_match(prompt, key):
+    # mykey = b'Z1QFxceGL_s6karbgfNFyuOdQ__m5TfHR7kuLPJChgs='
+    # enc = b'gAAAAABjRh0iNbsVb6_DKSHPmlg3jc4svMDEmKuYd-DcoTxEbESYI9F8tm8anjbsTsZYHz_avZudJDBdOXSHYZqKmhdoBcJd919hCffSMg6WFYP12hpvI7EeNppGFNoZsLGnDM5d6AOUeRVeIc2FbmB_j0vvcIwuEQ=='
+    # fernet = Fernet(mykey)
+    # openai.api_key = fernet.decrypt(enc).decode()
+    openai.api_key = key
+    response = openai.Completion.create(model="text-davinci-002", prompt=prompt, temperature=0.0, max_tokens=256)
+    result = response.choices[0].text.strip()
+    # print(result)
+    return result
+
+
+def read_text_from_file(text_path):
+    text_file = open(text_path, "r")
+    prompt = text_file.read()
+    return prompt
+
+
+# Get gpt-3 prompt with variables, ontology terms and match targets
+def get_prompt(vars, terms, target):
+    text_file = open("prompts/prompt.txt", "r")
+    prompt = text_file.read()
+    text_file.close()
+
+    vstr = ''
+    vlen = len(vars)
+    i = 1;
+    for v in vars:
+        vstr += str(i) + " (" + str(v[1]) + ", " + str(v[2]) + ")\n"
+        i += 1;
+    # print(vstr)
+    tstr = '[' + ', '.join(terms) + ']'
+    tlen = len(terms)
+    # print(tstr)
+    prompt = prompt.replace("[VAR]", vstr)
+    prompt = prompt.replace("[VLEN]", str(vlen))
+    prompt = prompt.replace("[TERMS]", tstr)
+    prompt = prompt.replace("[TLEN]", str(tlen))
+    prompt = prompt.replace("[TARGET]", target)
+    # print(prompt)
+    return prompt
+
+
+# Get gpt-3 prompt with formula, code terms and match formula targets
+def get_code_formula_prompt(code, formula, target):
+    text_file = open(os.path.join(os.path.dirname(__file__), 'prompts/code_formula_prompt.txt'), "r")
+    prompt = text_file.read()
+    text_file.close()
+
+    prompt = prompt.replace("[CODE]", code)
+    prompt = prompt.replace("[FORMULA]", formula)
+    prompt = prompt.replace("[TARGET]", target)
+    # print(prompt)
+    return prompt
+
+
+# Get gpt-3 prompt with formula, code terms and match formula targets
+def get_code_text_prompt(code, text, target):
+    text_file = open(os.path.join(os.path.dirname(__file__), 'prompts/code_text_prompt.txt'), "r")
+    prompt = text_file.read()
+    text_file.close()
+
+    prompt = prompt.replace("[CODE]", code)
+    prompt = prompt.replace("[TEXT]", text)
+    prompt = prompt.replace("[TARGET]", target)
+    # print(prompt)
+    return prompt
+
+
+# Get gpt-3 prompt with code, dataset and match function targets
+def get_code_dataset_prompt(code, dataset, target):
+    text_file = open(os.path.join(os.path.dirname(__file__), "prompts/code_dataset_prompt.txt"), "r")
+    prompt = text_file.read()
+    text_file.close()
+
+    prompt = prompt.replace("[CODE]", code)
+    prompt = prompt.replace("[DATASET]", dataset)
+    prompt = prompt.replace("[TARGET]", target)
+    # print(prompt)
+    return prompt
+
+
 
 def get_variables(path):
     list = []
@@ -145,10 +238,10 @@ def code_formula_connection(code, formulas, gpt_key):
         for t in flist:
             prompt = get_code_formula_prompt(code, formulas, t)
             match = get_gpt_match(prompt, gpt_key)
-            
+
             matches.append([t, match.split(":")[1]])
         return matches, True
-    except OpenAIError as err:   
+    except OpenAIError as err:
         return f"OpenAI connection error: {err}", False
 
 DEFAULT_TERMS = ['population', 'doubling time', 'recovery time', 'infectious time']
