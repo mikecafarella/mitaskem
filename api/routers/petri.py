f5dbcf39c16bb869ac46e5a14d31df1e739a6fe9
Markos Markakis <mmarkakis98@gmail.com>
2023-01-26 16:54:01 -0500

    Router for init params

--- api/routers/petri.py
+++ api/routers/petri.py
@@ -40,8 +40,17 @@ def run_transitions(code: str, gpt_key: str):
     return s
 
 @router.post("/match_place_to_text", tags=["Petri net"])
-def run_transitions(text: str, place: str, gpt_key: str):
+def run_match_place_to_text(text: str, place: str, gpt_key: str):
     s, success = match_place_to_text(text=text, place=place, gpt_key=gpt_key)
+
+    if not success:
+        return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content=s)
+
+    return s
+
+@router.post("/match_place_to_text", tags=["Petri net"])
+def run_init_param_from_text(text: str, param: str, gpt_key: str):
+    s, success = init_param_from_text(text=text, param=param, gpt_key=gpt_key)
 
     if not success:
         return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content=s)
