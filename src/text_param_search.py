2ec0da5648538ba33589513fac700ce2fff58144
Markos Markakis <mmarkakis98@gmail.com>
2023-01-30 15:38:58 -0500

    Text parameter search

--- src/text_param_search.py
+++ src/text_param_search.py
@@ -4,28 +4,34 @@ from connect import *
 import re
 import argparse
 
-GPT_KEY="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"
+GPT_KEY=""
 
 def text_param_search(text, gpt_key):
     try:
         prompt = get_text_param_prompt(text)
-        match = get_gpt_match(prompt, gpt_key)
+        match = get_gpt_match(prompt, gpt_key, "text-davinci-003")
         return match, True
     except OpenAIError as err:   
         return f"OpenAI connection error: {err}", False
 
 def main(args):
 
-    out_filename = args.out_dir + args.in_path.split("/")[-1].split(".")[0] + "_params.txt"
+    out_filename = args.out_dir + "/" + args.in_path.split("/")[-1].split(".")[0] + "_params.txt"
 
 
     with open(args.in_path, "r") as fi, open(out_filename, "w+") as fo:
         text = fi.read()
+        length = len(text)
+        segments = int(length/3500 + 1)
 
-        text = text[:3000]
-
-        output = text_param_search(text, GPT_KEY)
-        print(output)   
+        for i in range(segments):
+            snippet = text[i * 3500: (i+1) * 3500]
+            #print("SNIPPET: " + snippet)
+            output, success = text_param_search(snippet, GPT_KEY)
+            if success:
+                print("OUTPUT: " + output + "\n------\n")  
+                if output != "None":
+                    fo.write(output + "\n") 
 
 
 if __name__=="__main__":
