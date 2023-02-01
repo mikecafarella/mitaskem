87bbc5757319175d3f40446b14eb8466f4cd7965
Markos Markakis <mmarkakis98@gmail.com>
2023-01-31 23:19:27 -0500

    Escape strings

--- src/text_search.py
+++ src/text_search.py
@@ -52,9 +52,9 @@ def vars_to_json(var_dict: dict) -> str:
     id = 0
 
     for var_name in var_dict:
-        var_defs_s = "[" + ','.join(i for i in var_dict[var_name]) + "]"
+        var_defs_s = "[\"" + '\",\"'.join(i for i in var_dict[var_name]) + "\"]"
         var_ground = get_mira_dkg_term(var_name, ['id', 'name'])
-        var_ground_s = "[" + ",".join([("[" + ",".join([str(item) for item in sublist]) + "]") for sublist in var_ground]) + "]"
+        var_ground_s = "[" + ",".join([("[\"" + "\",\"".join([str(item) for item in sublist]) + "\"]") for sublist in var_ground]) + "]"
 
         if is_first:
             is_first = False
