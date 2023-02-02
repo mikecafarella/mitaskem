e4a7446976bdcceab1631012e53c21c74dbec48d
Markos Markakis <mmarkakis98@gmail.com>
2023-02-02 12:11:59 -0500

    Add endpoint for dataset linking

--- api/routers/annotation.py
+++ api/routers/annotation.py
@@ -9,7 +9,7 @@ sys.path.append(
     os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 )
 from src.text_search import text_var_search, vars_to_json, vars_dedup
-from src.connect import vars_formula_connection
+from src.connect import vars_formula_connection, vars_dataset_connection
 from src.link_annos_to_pyacset import link_annos_to_pyacset
 
 router = APIRouter()
@@ -33,6 +33,15 @@ def find_variables_from_text(text: str, gpt_key: str):
         outputs += s
 
     return ast.literal_eval(vars_to_json(vars_dedup(outputs)))
+
+@router.post("/link_datasets_to_vars", tags=["Paper-2-annotated-vars"])
+def link_dataset_columns_to_extracted_variables(json_str: str, dataset_str: str, gpt_key: str):
+    s, success = vars_dataset_connection(json_str=json_str, dataset_str=dataset_str, gpt_key=gpt_key)
+    
+    if not success:
+        return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content=s)
+
+    return ast.literal_eval(s)
 
 @router.post("/link_latex_to_vars", tags=["Paper-2-annotated-vars"])
 def link_latex_formulas_to_extracted_variables(json_str: str, formula: str, gpt_key: str):
