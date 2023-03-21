1b82adc3500609ea5e5d3de85a2b95cea71fe3c3
chunwei <chunwei@uchicago.edu>
2023-03-21 14:09:01 -0700

    update march demo

--- src/link_annos_to_pyacset.py
+++ src/link_annos_to_pyacset.py
@@ -4,10 +4,62 @@ import json
 import unicodedata as ud
 import ast
 
+greek_alphabet = {
+    u'\u0391': 'Alpha',
+    u'\u0392': 'Beta',
+    u'\u0393': 'Gamma',
+    u'\u0394': 'Delta',
+    u'\u0395': 'Epsilon',
+    u'\u0396': 'Zeta',
+    u'\u0397': 'Eta',
+    u'\u0398': 'Theta',
+    u'\u0399': 'Iota',
+    u'\u039A': 'Kappa',
+    u'\u039B': 'Lamda',
+    u'\u039C': 'Mu',
+    u'\u039D': 'Nu',
+    u'\u039E': 'Xi',
+    u'\u039F': 'Omicron',
+    u'\u03A0': 'Pi',
+    u'\u03A1': 'Rho',
+    u'\u03A3': 'Sigma',
+    u'\u03A4': 'Tau',
+    u'\u03A5': 'Upsilon',
+    u'\u03A6': 'Phi',
+    u'\u03A7': 'Chi',
+    u'\u03A8': 'Psi',
+    u'\u03A9': 'Omega',
+    u'\u03B1': 'alpha',
+    u'\u03B2': 'beta',
+    u'\u03B3': 'gamma',
+    u'\u03B4': 'delta',
+    u'\u03B5': 'epsilon',
+    u'\u03B6': 'zeta',
+    u'\u03B7': 'eta',
+    u'\u03B8': 'theta',
+    u'\u03B9': 'iota',
+    u'\u03BA': 'kappa',
+    u'\u03BB': 'lamda',
+    u'\u03BC': 'mu',
+    u'\u03BD': 'nu',
+    u'\u03BE': 'xi',
+    u'\u03BF': 'omicron',
+    u'\u03C0': 'pi',
+    u'\u03C1': 'rho',
+    u'\u03C3': 'sigma',
+    u'\u03C4': 'tau',
+    u'\u03C5': 'upsilon',
+    u'\u03C6': 'phi',
+    u'\u03C7': 'chi',
+    u'\u03C8': 'psi',
+    u'\u03C9': 'omega',
+}
+
 
 def link_annos_to_pyacset(pyacset_s, annos_s, info_s=""):
     pyacset = ast.literal_eval(pyacset_s)
     annos = ast.literal_eval(annos_s)
+    greek_set = {greek_alphabet[sub].upper() for sub in greek_alphabet}
     if info_s != "":
         info = ast.literal_eval(info_s)
 
@@ -47,11 +99,13 @@ def link_annos_to_pyacset(pyacset_s, annos_s, info_s=""):
     for tr in transitions:
         name = tr["tname"].lstrip().rstrip()
         uid = tr["uid"]
-
-        d[uid] = var_d.get(ud.lookup(f"GREEK SMALL LETTER {name.upper()}"), {})
+        if name.upper() in greek_set:
+            d[uid] = var_d.get(ud.lookup(f"GREEK SMALL LETTER {name.upper()}"), {})
+        else:
+            d[uid] = var_d.get(name, {})
     
     # Add in paper and doi info
-    if info != "":
+    if info_s != "":
         for item in d:
             contents = d[item]
             contents["file"] = info["pdf_name"]
